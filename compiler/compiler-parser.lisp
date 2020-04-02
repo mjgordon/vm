edd91ba4aa1172c997e428931bd4ab7102598c96
matt <->
2020-04-02 22:54:57 +0200

    C CM : Parser passes around results using (values) and (multiple-value-bind) instead of as lists now

--- compiler/compiler-parser.lisp
+++ compiler/compiler-parser.lisp
@@ -5,6 +5,27 @@
 - Subtree : A list of tokens at an arbitrary level of the AST. Usually comes from a rule expansion
 
 
+             +-----+          
+             |PARSE|          
+             +-----+          
+                |             
+                v             
+         +-------------+      
+     +-->|PARSE-SUBTREE|      
+     |   +-------------+      
+     |     |        |         
+     |     v        |         
++----------------+  |         
+|PARSE-FOR-BRANCH|  |         
++----------------+  |         
+     ^              v         
+     |     +-----------------+
+     +---- |PARSE-FOR-OPTIONS|
+           +-----------------+
+                              
+                              
+                              
+
 
 === Structure ===
 - Most parser operations return a list of 
@@ -77,9 +98,8 @@ Returns nil if token-name is not a rule"
 
 (defmacro if-branch (test-fun branch tokens criticality &optional (success-form nil) (failure-form nil))
   "Specialized if form. Makes checking for successful branch completions easier"
-  `(let* ((b-check (funcall ,test-fun ,branch ,tokens ,criticality))
-	  (b-result (first b-check))
-	  (b-tokens (second b-check)))
+  `(multiple-value-bind (b-result b-tokens)
+       (funcall ,test-fun ,branch ,tokens ,criticality)
      (if b-result
 	 (progn
 	   (setf ,tokens b-tokens)
@@ -87,77 +107,77 @@ Returns nil if token-name is not a rule"
 	   b-result)
 	 ,failure-form)))
 
-;; TODO create macro that assigns *let specific names to the first and second items in a list
+
 (defun parse-for-branch (branch tokens critical)
   "Check a single branch in a subtree. This branch may either describe a rule, or be a single normal token"
   (let ((rule (get-rule branch)))
     (if rule
 	;; If a rule, call parse subtree again
-	(let* ((rule-subtree (parse-subtree rule tokens critical))
-	       (rule-results (first rule-subtree))
-	       (rule-tokens (second rule-subtree)))
+	(multiple-value-bind (rule-results rule-tokens)
+	    (parse-subtree rule tokens critical)
 	  ;; Check that the results of the rule expansion are not an empty list, a list of all nils, or 'repeat?
 	  ;; Wait why would it be just 'repeat? hmmm
 	  (if (and rule-results
 		   (not (list-all-nil rule-results))
 		   (not (eq rule-results 'repeat)))
 	      ;; If the rule is completed, add it as a token with its expansion as its value
-	      (list (make-token :type branch :value (remove-multiple '(repeat syntax) rule-results))
-		    rule-tokens)
+	      (values (make-token :type branch :value (remove-multiple '(repeat syntax) rule-results))
+		      rule-tokens)
 	      ;; Else return nil
-	      (list nil tokens)))
+	      (values nil tokens)))
 	;; Else, check the first token in the list against the requested type
 	;; The logic for every token eventually arrives here. 
 	(if (eq (token-type (first tokens)) branch)
 	    ;; If the matching symbol is found, either return it or a syntax maker
-	    (list (if (token-semantic (first tokens))
-		      (first tokens)
-		      'syntax)
-		  (rest tokens))
+	    (values (if (token-semantic (first tokens))
+			(first tokens)
+			'syntax)
+		    (rest tokens))
 	    ;; Else return nil and log an error
 	    (progn
 	      (when critical
 		(print branch)
 		(log-error (get-error-type branch) (first tokens)))
-	      (list nil tokens))))))
+	      (values nil tokens))))))
 	    
 (defun parse-for-options (options tokens critical)
   "Check the branches in an options list.
 Returns the first options that succeeds. Otherwise returns nil"
   (loop for option in options do
-       (let ((parse-result (parse-for-branch option tokens nil)))
-	 (when (first parse-result)
-	   (return-from parse-for-options parse-result))))
+       (multiple-value-bind (option-result option-tokens)
+	   (parse-for-branch option tokens nil)
+	 (when option-result
+	   (return-from parse-for-options (values option-result option-tokens)))))
   ;; If none of the options succeed and the option list is critical, log an error.
   ;; Note the individual options never had criticality"
   (when critical
     (log-error 'error-options-failed options))
-  (list nil tokens))
+  (values nil tokens))
 
 
 (defun parse-subtree (subtree tokens critical)
   "Parses a list of input tokens (usually from a rule expansion)
 These tokens will either be rule name, list of options, or normal token, and may be repeating.
 Returns  (result-tokens remaining-source-tokens)"
-  (list (loop while subtree collect
-	     (let ((branch (pop subtree)))
-	       (cond
-		 ;; Current branch is a repeat
-		 ((eq '& (first subtree))
-		  (if-branch #'parse-for-branch branch tokens nil
-			     (push branch subtree)
-			     (progn
-			       (pop subtree)
-			       'repeat)))
-		 ;; Current branch is an option list
-		 ((listp branch)
-		  (if-branch #'parse-for-options branch tokens critical))
-		 ;; Default - Current branch is normal token or rule name
-		 (t
-		  (if-branch #'parse-for-branch branch tokens critical
-			     nil
-			     (return-from parse-subtree (list nil tokens)))))))
-	tokens))
+  (values (loop while subtree collect
+	       (let ((branch (pop subtree)))
+		 (cond
+		   ;; Current branch is a repeat
+		   ((eq '& (first subtree))
+		    (if-branch #'parse-for-branch branch tokens nil
+			       (push branch subtree)
+			       (progn
+				 (pop subtree)
+				 'repeat)))
+		   ;; Current branch is an option list
+		   ((listp branch)
+		    (if-branch #'parse-for-options branch tokens critical))
+		   ;; Default - Current branch is normal token or rule name
+		   (t
+		    (if-branch #'parse-for-branch branch tokens critical
+			       nil
+			       (return-from parse-subtree (values nil tokens)))))))
+	  tokens))
 	     
 
 ;; TODO: May be able to take out the remove-multiple here? 
@@ -166,4 +186,4 @@ Returns  (result-tokens remaining-source-tokens)"
   (when *verbose*
     (format t "~a~%" tokens))
   (let ((program-tree '(<program> .())))
-    (remove-multiple '(syntax repeat) (first (parse-subtree program-tree tokens t)))))
+    (remove-multiple '(syntax repeat) (parse-subtree program-tree tokens t))))
